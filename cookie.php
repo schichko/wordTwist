7e4880fbb6da13a45e7bb3749cf2daf41fc0c082
Jake Schichko <schichko@udel.edu>
2019-09-19 22:45:08 -0400

    Adding more cookies

--- cookie.php
+++ cookie.php
@@ -2,13 +2,18 @@
 session_start();
 
 
+
 if (!isset($_SESSION["name"])){
     if($_POST['receiver_name'] != null){
         $_SESSION["name"] = $_POST['receiver_name'];
     }
+    if($_POST['rack'] != null){
+        $_SESSION["rack"] = $_POST['rack'];
+    }
 } else {
     header('HTTP/1.1 200 OK');
     header('Content-Type: application/json');
-    echo json_encode($_SESSION["name"]);
+    $dbSigning_array = array($_SESSION["name"],$_SESSION["rack"]);
+    echo json_encode($dbSigning_array);
 }
 ?>
