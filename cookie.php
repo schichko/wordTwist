86cab8621bd0334f540f42653c9f7587d0988727
Jake Schichko <schichko@udel.edu>
2019-09-19 23:08:44 -0400

    Fix

--- cookie.php
+++ cookie.php
@@ -10,10 +10,13 @@ if (!isset($_SESSION["name"])){
     if($_POST['rack'] != null){
         $_SESSION["rack"] = $_POST['rack'];
     }
+    if($_POST['wordList'] != null){
+        $_SESSION["wordList"] = $_POST['wordList'];
+    }
 } else {
     header('HTTP/1.1 200 OK');
     header('Content-Type: application/json');
-    $dbSigning_array = array($_SESSION["name"],$_SESSION["rack"]);
+    $dbSigning_array = array($_SESSION["name"],$_SESSION["rack"],$_SESSION["wordList"]);
     echo json_encode($dbSigning_array);
 }
 ?>
